a9e0e215f1ae168d8ffd2a5e125b71587a746d89
wahyu <wahyuritchie@gmail.com>
2020-10-08 09:03:27 +0700

    first commit

---
+++ src/main/java/why/store/entity/Product.java
@@ -0,0 +1,46 @@
+package why.store.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.sun.istack.NotNull;
+import lombok.Data;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@Data
+@Entity
+@Table(name = "m_product")
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties(value = {"createdAt", "updatedAt"}, allowGetters = true)
+public class Product extends BaseEntity {
+
+    @NotNull
+    String name;
+
+    @NotNull
+    String price;
+
+    @NotNull
+    Integer stock;
+
+    @ManyToOne(fetch= FetchType.LAZY)
+    @Fetch(FetchMode.JOIN)
+    @JoinColumn(name="categoryId")
+    Category category;
+
+    @Column(nullable = false, updatable = false)
+    @CreatedDate
+    private Date createdAt;
+
+    @Column(nullable = false)
+    @LastModifiedDate
+    private Date updatedAt;
+
+}
